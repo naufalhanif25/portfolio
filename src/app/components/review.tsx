a79a19ee700a6ba4f13251da2b5f8a6fe72b3c0e
naufalhanif25 <naufal.hanif2525@gmail.com>
2025-07-21 22:16:04 +0700

    fix: component styles

--- src/app/components/review.tsx
+++ src/app/components/review.tsx
@@ -11,10 +11,10 @@ export function Review({
 }) {
     return (
         <span className="jaguar-display rounded-[12px] gap-[8px] w-[168px] py-[16px] flex flex-col items-center justify-center">
-            <h1 className="text-[11pt] font-medium text-nowrap text-center">{title}</h1>
+            <h4 className="text-[11pt] font-medium text-nowrap text-center">{title}</h4>
             <span className="flex flex-row items-center justify-center gap-[16px]">
                 {icon}
-                <h1 className="text-[24pt] font-semibold">{value}</h1>
+                <h5 className="text-[24pt] font-semibold">{value}</h5>
             </span>
         </span>
     );
